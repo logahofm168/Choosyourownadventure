67fd4d9633f2d83493977fec6c62b4b2f2822d3c
logahofm168 <logahofm168@SDS-L-DMFDF63>
2024-10-29 11:18:15 -0400

    Add project files.

---
+++ Choosyourownadventure/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Choosyourownadventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outPutlable = new System.Windows.Forms.Label();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option1Lable = new System.Windows.Forms.Label();
+            this.option2Lable = new System.Windows.Forms.Label();
+            this.option3Lable = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outPutlable
+            // 
+            this.outPutlable.BackColor = System.Drawing.Color.Transparent;
+            this.outPutlable.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outPutlable.ForeColor = System.Drawing.Color.Black;
+            this.outPutlable.Location = new System.Drawing.Point(-3, 2);
+            this.outPutlable.Name = "outPutlable";
+            this.outPutlable.Size = new System.Drawing.Size(626, 190);
+            this.outPutlable.TabIndex = 0;
+            this.outPutlable.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackColor = System.Drawing.Color.Transparent;
+            this.option2Button.BackgroundImage = global::Choosyourownadventure.Properties.Resources.Maybe_a_Horror_Game_Arrow;
+            this.option2Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option2Button.FlatAppearance.BorderSize = 0;
+            this.option2Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.option2Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.option2Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2Button.Location = new System.Drawing.Point(15, 365);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(59, 44);
+            this.option2Button.TabIndex = 2;
+            this.option2Button.UseVisualStyleBackColor = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.BackColor = System.Drawing.Color.Transparent;
+            this.option3Button.BackgroundImage = global::Choosyourownadventure.Properties.Resources.Maybe_a_Horror_Game_Arrow;
+            this.option3Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option3Button.FlatAppearance.BorderSize = 0;
+            this.option3Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.option3Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.option3Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option3Button.Location = new System.Drawing.Point(15, 434);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(59, 48);
+            this.option3Button.TabIndex = 3;
+            this.option3Button.UseVisualStyleBackColor = false;
+            this.option3Button.Visible = false;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // option1Lable
+            // 
+            this.option1Lable.BackColor = System.Drawing.Color.Transparent;
+            this.option1Lable.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Lable.ForeColor = System.Drawing.Color.Black;
+            this.option1Lable.Location = new System.Drawing.Point(80, 319);
+            this.option1Lable.Name = "option1Lable";
+            this.option1Lable.Size = new System.Drawing.Size(258, 24);
+            this.option1Lable.TabIndex = 4;
+            // 
+            // option2Lable
+            // 
+            this.option2Lable.BackColor = System.Drawing.Color.Transparent;
+            this.option2Lable.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Lable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.option2Lable.Location = new System.Drawing.Point(80, 385);
+            this.option2Lable.Name = "option2Lable";
+            this.option2Lable.Size = new System.Drawing.Size(258, 24);
+            this.option2Lable.TabIndex = 5;
+            // 
+            // option3Lable
+            // 
+            this.option3Lable.BackColor = System.Drawing.Color.Transparent;
+            this.option3Lable.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Lable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.option3Lable.Location = new System.Drawing.Point(80, 458);
+            this.option3Lable.Name = "option3Lable";
+            this.option3Lable.Size = new System.Drawing.Size(258, 24);
+            this.option3Lable.TabIndex = 6;
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackColor = System.Drawing.Color.Transparent;
+            this.option1Button.BackgroundImage = global::Choosyourownadventure.Properties.Resources.Maybe_a_Horror_Game_Arrow;
+            this.option1Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option1Button.FlatAppearance.BorderSize = 0;
+            this.option1Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.option1Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.option1Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1Button.ForeColor = System.Drawing.Color.Transparent;
+            this.option1Button.Location = new System.Drawing.Point(15, 295);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(59, 48);
+            this.option1Button.TabIndex = 1;
+            this.option1Button.UseVisualStyleBackColor = false;
+            this.option1Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::Choosyourownadventure.Properties.Resources.pyramind_head;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(622, 494);
+            this.Controls.Add(this.option3Lable);
+            this.Controls.Add(this.option2Lable);
+            this.Controls.Add(this.option1Lable);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outPutlable);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Maybe A Horror Game ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outPutlable;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label option1Lable;
+        private System.Windows.Forms.Label option2Lable;
+        private System.Windows.Forms.Label option3Lable;
+    }
+}
+
---
+++ Choosyourownadventure/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Choosyourownadventure
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            outPutlable.Text = "You wake up in the middle of a road. " +
+                        "Looking around you see a sign that reads ''Silent Hill,Main'' ";
+            option1Lable.Text = "walk forwards (up the road)?";
+            option2Lable.Text = "walk back (down the road)?";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 3)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 5)
+            {
+                page = 9;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 13;
+            }
+            else if (page == 10)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 1;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 15)
+            {
+                page = 18;
+            }
+            else if (page == 16)
+            {
+                page = 1;
+            }
+            else if (page == 17)
+            {
+                page = 1;
+            }
+            else if (page == 18)
+            {
+                page = 1;
+            }
+            else if (page == 19)
+            {
+                page = 1;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+
+            displayPage();
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            //if statements
+            if (page == 1)
+            {
+                Random randomGenrator = new Random();
+                int randomValue = randomGenrator.Next(1, 101);
+
+                if (randomValue > 75)
+                {
+                    page = 8;
+                }
+                else
+                {
+                    page = 3;
+
+                }
+
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 21;
+            }
+            else if (page == 4)
+            {
+                page = 7;
+            }
+            else if (page == 5)
+            {
+                page = 10;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 14;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if (page == 11)
+            {
+                page = 21;
+            }
+            else if (page == 12)
+            {
+                page = 21;
+            }
+            else if (page == 13)
+            {
+                page = 17;
+            }
+            else if (page == 14)
+            {
+                page = 21;
+            }
+            else if (page == 15)
+            {
+                Random randomGenrator = new Random();
+                int randomValue = randomGenrator.Next(1, 101);
+
+                if (randomValue > 78)
+                {
+                    page = 19;
+                }
+                else
+                {
+                    page = 20;
+
+                }
+            }
+            else if (page == 16)
+            {
+                page = 21;
+            }
+            else if (page == 17)
+            {
+                page = 21;
+            }
+            else if (page == 18)
+            {
+                page = 21;
+            }
+            else if (page == 19)
+            {
+                page = 21;
+            }
+            else if (page == 20)
+            {
+                page = 21;
+            }
+
+            displayPage();
+
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+            if (page == 13)
+            {
+                page = 16;
+            }
+
+            displayPage();
+        }
+
+
+        public void displayPage()
+        {
+            switch (page)
+            {
+                case 1:
+
+                    BackgroundImage = Properties.Resources.pyramind_head;
+
+                   outPutlable.Text = "You wake up in the middle of a road. " +
+                        "Looking around you see a sign that reads ''Silent Hill,Main'' ";
+                        
+                    option1Lable.Text = "walk forwards (up the road)?";
+                    option2Lable.Text = "walk back (down the road)?";
+                    break;
+                case 2:
+
+                    BackgroundImage = Properties.Resources.silent_hil_town;
+
+                    outPutlable.Text = "Going forwards you eventually enter a town. You see two paths, " +
+                        "do you go down the path in front of you, or the one to the right? ";
+
+                    option1Lable.Text = "Go forward?";
+                    option2Lable.Text = "Go right?";
+                    break;
+                case 3:
+
+                    BackgroundImage = Properties.Resources.pyramind_head;
+
+                   outPutlable.Text = "You keep walking south down the road, and over and over again you come back to the sign," +
+                        " you eventually die of exhaustion, play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 4:
+
+                    BackgroundImage = Properties.Resources.hospital;
+
+                    outPutlable.Text = "Going right you eventually encounter a hospital, do you go in?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 5:
+
+                    BackgroundImage = Properties.Resources.silent_hill_elementry_school;
+
+                    outPutlable.Text = "Going forwards you eventually encounter an old high school, do you decide to go in?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 6:
+
+
+
+                    outPutlable.Text = "Entering the hospital the door slams behind you causing these puppet like nurses" +
+                        " to rush towards you ripping you apart, you die cold and alone. play again? ";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 7:
+                    outPutlable.Text = "Deciding to not enter the hospital you walk away tripping on a rock hitting your head, play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 8:
+
+                    BackgroundImage = Properties.Resources.pyramind_head;
+
+                    outPutlable.Text = "Going south the fog  dissipates eventually leading you to your freedom, play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 9:
+
+
+
+                    outPutlable.Text = "Deciding to enter the school it is immediately apparent that no one has been here in ages. Looking around you enter a gym which almost looks like it was set for a promb, though it looks like very one just dissapperad. " +
+                        "After some time you reach the second floor as you do a siren which almost sounds like a fire trucks wails outside though nothing's there," +
+                        " do you want to investigate farther.";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 10:
+
+
+
+                    outPutlable.Text = "Deciding not to go into the school, " +
+                        "you hear a screech from behind, as these giant bugs rush towards you in a swarm, do you run?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 11:
+
+
+
+                    outPutlable.Text = "Running from the bugs you eventually reach a church, but just before you reach the church steps a man with a pyramid on his head cleaves you in half, " +
+                        "before everything goes black you see someone peek their head out the church doors just to stare, play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 12:
+
+
+
+                    outPutlable.Text = "Deciding not to run you except your fate that it's likely you'll never get home," +
+                        " falling to your knees you let the bugs swarm you, play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 13:
+
+
+
+                    outPutlable.Text = "Deciding to investigate further you exit the room only for what seems to be a giant meat cleaver" +
+                        " to get caught in the door way above your head. do you run, fight or freeze?";
+
+                    option1Lable.Text = "Run?";
+                    option2Lable.Text = "Fight?";
+                    option3Lable.Text = "Freez?";
+                    option3Button.Visible = true;
+
+                    break;
+                case 14:
+                    
+
+
+                    outPutlable.Text = "Deciding not to investigate further you turn away from the window, " +
+                        "only for the world around you to be warped as everything has been covered in a rust-like coating." +
+                        " This causes you to rush out of the room only for you to fall, the last thing you see before blacking out is a man with a pyramid headed" +
+                        " dragging you away with no lower body. play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    option3Lable.Text = "";
+                    break;
+                case 15:
+
+
+
+                    outPutlable.Text = "Deciding to run for your life you duck under him and you run down the hallway, " +
+                        "as you do the world around begins to become warped as everything has been covered in a rust-like coating, " +
+                        "continuing to run down the hall you see the main hall is covered in wire with a small hole to the side, and the other is a door that leads around it. which do you pick? ";
+
+                    option1Lable.Text = "Main Hall";
+                    option2Lable.Text = "Class room door?";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    break;
+                case 16:
+
+
+
+                    outPutlable.Text = "You sit there frozen, confused the pyramid headed man pokes you before shrugging and leaving you alone to move on to something else, " +
+                        "turns out you were so shocked you had a heart attack and died on the spot. Play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    break;
+                case 17:
+
+
+
+                    outPutlable.Text = "Deciding to fight this man you punch him in the torso hurting your hand so much that you have to take a second to recover, " +
+                        "this was enough time for them to unwedge their giant meat cleaver from the door frame and attack you cutting you in two. Play again?  ";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    break;
+                case 18:
+
+
+
+                    outPutlable.Text = "Deciding to duck into the room, " +
+                        "you realize that the door leading out of the room is locked, " +
+                        "looking back the pryamind headed man is already at the door, and theres no way to get passed him, play again? ";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    break;
+                case 19:
+
+
+
+                    outPutlable.Text = "Deciding to duck though the hole in the wire, you successfully get though with minimal scratching. " +
+                        "Continuing to run you burst out the front doors of the school with the pyramid headed man on your heel,franticly look around you  " +
+                        "set eyes on small bar. Hiding in the bar you hear the man walk by covering your mouth hoping he wouldn't find you, " +
+                        "you peek over the edge only to meet your own face, but with the body of the pryamide headed man. Play again?";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    break;
+                case 20:
+
+
+
+                    outPutlable.Text = "Deciding to try and duck through the hole in the wire, you get caught on the wire looking back you see the pyramid headed man getting closer, " +
+                        "in your struggle to get free the wire digs deeper into your skin causing you to slowly bleed out. play again?  ";
+
+                    option1Lable.Text = "Yes?";
+                    option2Lable.Text = "No?";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    break;
+                case 21:
+                    outPutlable.Text = "Thank you for playing ";
+
+                    option1Lable.Text = "...";
+                    option2Lable.Text = "...";
+                    option3Lable.Text = "";
+                    option3Button.Visible = false;
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
